fd76aa175c2d04274921337c2d991f858090f2c6
Your Name <you@example.com>
2019-05-10 19:27:50 -0700

    everything working except radio button unchecking

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -3,16 +3,36 @@ $(document).ready(function() {
     //variables
     var rightAnswers = 0; //Number of questions answered correctly
     var wrongAnswers = 0; //Number of questions answered incorrectly
+    var time= 35;         //Number of seconds to be counted down during quiz
+    var stopTime;         //Timer variable
 
-    $("#playGround").hide();
-    $("#finished").hide();
+    //$(".titleContainer").hide();
+    $(".playGround").hide();
+    $(".finished").hide();
+    $(".oops").hide();
 
     
     //Function that opens the game
     var playGame = function() {
-        console.log(1 + 2);
-        $("#start").hide();
-        $("#playGround").show();
+        $(".titleContainer").hide();
+        $(".oops").hide();
+        $(".playGround").show();
+        timer();
+    }
+
+    //Function that runs the timer
+    var timer = function() {
+        stopTime = setTimeout(timer, 1000);
+        $("#countDown").html(time);
+
+        if (time == -1) {
+            $(".playGround").hide();
+            $(".oops").show();
+            clearTimeout(stopTime);
+            time=36;
+        }
+
+        time--;
     }
 
     //Function that tallies up the right and wrong answers
@@ -33,16 +53,16 @@ $(document).ready(function() {
 
     //function that finsihes the game
     var gameDone = function () {
-        $("#playGround").hide();
-        $("#finished").show();
+        $(".playGround").hide();
+        $(".finished").show();
         $("#rightNum").html(rightAnswers);
         $("#wrongNum").html(wrongAnswers);
         wellPlayed();
     }
 
     var wellPlayed = function() {
-        if (rightAnswers >= 5) {
-            $('#gameWellPlayed').html("<h2>WELCOME TO RIVENDELL, MR. BAGGINS!</h2>");
+        if (rightAnswers > 5) {
+            $('#gameWellPlayed').html("<h2>WELCOME TO RIVENDELL!</h2>");
             $('#endGameImage').append("<img id='elrondImage' src='assets/images/elrond.jpg'>"); 
         } else {
             $('#gameWellPlayed').html("<h2>BEGONE! SERVANT OF THE DARK LORD!</h2>");
@@ -59,7 +79,32 @@ $(document).ready(function() {
     whatAreTheAnswers();
 
     //Finish the game and see the score
-    $('#done').click(gameDone);
+    $('#done').click(function() {
+        clearTimeout(stopTime);
+        gameDone();
+    });
+
+   //Restarts the game after the time runs out
+
+   $("#reStart").click(function() {
+        rightAnswers = 0;
+        wrongAnswers = 0;
+        playGame();
+    });
+
+    //Restarts game from .finished div
+    
+    $("#tryAgain").click(function() {
+        rightAnswers = 0;
+        wrongAnswers = 0;
+        time = 35;
+        $("#ringwraithsImage").detach();
+        $("#elrondImage").detach();
+        $(".finished").hide();
+        playGame();
+    });
+
+
 
    
 
